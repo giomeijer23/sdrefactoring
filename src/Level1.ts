40147438ae26400f749deae0def71295d5952c7b
Beregeluk <renzo.debeer06@gmail.com>
2023-12-18 09:55:56 +0100

    Level 1 aangemaakt als je op E in open wereld klikt ga je er naartoe

--- src/Level1.ts
+++ src/Level1.ts
@@ -1,28 +1,18 @@
+/* eslint-disable max-len */
 import CanvasRenderer from './CanvasRenderer.js';
 import KeyListener from './KeyListener.js';
-import Level1 from './Level1.js';
 import MouseListener from './MouseListener.js';
 import Scene from './Scene.js';
 
-export default class OpenWereld extends Scene {
-  private starting: boolean;
-
-  private keylistener: KeyListener;
-
-  private logo: HTMLImageElement;
-
+export default class Level1 extends Scene {
   private goToNextScene: boolean;
 
-  private posX: number;
-
-  private posY: number;
+  private logo: HTMLImageElement;
 
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
     this.goToNextScene = false;
-    this.posX = 100;
-    this.posY = 100;
-    this.logo = CanvasRenderer.loadNewImage('./assets/MicrosoftTeams-image (11).png');
+    this.logo = CanvasRenderer.loadNewImage('./assets/blue-dungeon.png');
   }
 
   /**
@@ -30,23 +20,28 @@ export default class OpenWereld extends Scene {
    * @param mouseListener
    */
   public override processInput(mouseListener: MouseListener): void {
-
-  }
-
-
-  public override processInput2(keylistener: KeyListener): void {
-    if (keylistener.isKeyDown('KeyE')) {
+    if (mouseListener.buttonPressed(MouseListener.BUTTON_LEFT)) {
       this.goToNextScene = true;
     }
   }
+
   /**
    *
-   * @param elapsed
+   * @param keylistener t
    */
-  public override update(elapsed: number): void {
+  public override processInput2(keylistener: KeyListener): void {
+    // Hoeft niks te doen
   }
 
-  public override getNextScene(): Scene | null {
+  /**
+   *
+   * @param elapsed t
+   */
+  public override update(elapsed: number): void {
+    // Hoeft niks te doen
+  }
+
+  public override getNextScene(): Scene {
     if (this.goToNextScene) {
       return new Level1(this.maxX, this.maxY);
     }
@@ -55,10 +50,17 @@ export default class OpenWereld extends Scene {
 
   /**
    *
-   * @param canvas
+   * @param canvas t
    */
   public override render(canvas: HTMLCanvasElement): void {
-    CanvasRenderer.fillCanvas(canvas, 'rgb(231, 206, 162)');
-    CanvasRenderer.drawImage(canvas, this.logo, canvas.width / 2 - this.logo.width / 2, canvas.height / 1.85 - this.logo.height / 2);
+    CanvasRenderer.fillCanvas(canvas, 'rgb(70, 106, 44)');
+    CanvasRenderer.drawImage(canvas, this.logo, canvas.width / 2 - this.logo.width / 2, canvas.height / 2.1 - this.logo.height / 2);
   }
+
+  // /**
+  //  *
+  //  */
+  // public nextLevel(): Level {
+  //   // Hoeft niks te doen
+  // }
 }
